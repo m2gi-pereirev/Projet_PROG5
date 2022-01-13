aedeada88ff3b75313c162bda4212227a231ff1c
Benny <benny.bellier@gmail.com>
2022-01-13 04:00:58 +0100

    Ajout script routine test automatique

--- linker/linker.c
+++ linker/linker.c
@@ -2,17 +2,19 @@
 
 int main(int argc, char **argv)
 {
-  char *file_content = NULL;
-  Elf32_file elf;
+  if (argc < 3)
+    printf("Usage: %s elf_file file_out\n", argv[0]);
+  else
+  {
+    char *file_content = NULL;
+    Elf32_file elf;
 
-  char *filename1 = "test/example1.o";
-  char *filename2 = "test.o";
-
-  file_content = load_file_content(filename1);
-  storage_elf_content(file_content, &elf);
-  relocations_execution(&elf);
-  elf_reorganization(&elf);
-  FILE *file_out = load_elf_output(filename2);
-  write_elf_content(&elf, file_out);
-  free_elf32_file(&elf);
+    file_content = load_file_content(argv[1]);
+    storage_elf_content(file_content, &elf);
+    relocations_execution(&elf);
+    elf_reorganization(&elf);
+    FILE *file_out = load_elf_output(argv[2]);
+    write_elf_content(&elf, file_out);
+    free_elf32_file(&elf);
+  }
 }
